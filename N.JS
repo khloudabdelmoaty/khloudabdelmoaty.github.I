435ae7c7351ce1a35d93bd44985b5723557cf2b3
khloudabdelmoaty <kholod.abdelmoaty@gmail.com>
2021-05-24 23:09:10 +0200

    first commit

---
+++ N.JS
@@ -0,0 +1,7 @@
+var banner = document.getElementsByClassName('banner') [0];
+         var blocks = document.getElementsByClassName('blocks');
+
+         for ( var i=0; i < 400; i++){
+             banner.innerHTML += "<div class='blocks'> </div> ";
+             blocks[i].style.animationDelay = `${ i * 0.05}s`;
+         }
